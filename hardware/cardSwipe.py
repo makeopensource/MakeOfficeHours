ad430f69e45e960b7f6ed93d39aa5588f7eec073
mariam-al0 <166653306+mariam-al0@users.noreply.github.com>
2025-08-29 17:43:46 -0400

    Init commit

---
+++ hardware/cardSwipe.py
@@ -0,0 +1,25 @@
+import os
+
+
+def polling():
+    #file = open('hardware.txt', 'r')
+    #read = file.readlines()
+    #while len(read) == 0:
+        #file = open('hardware.txt', 'r')
+        #read = file.readlines()
+
+    #print('stuff')
+    file = open('hardware.txt', 'r')
+    read = file.readlines()
+    size = len(read)
+
+    while size == 0:
+        file.close()
+        file = open('hardware.txt', 'r')
+        read = file.readlines()
+        size = len(read)
+
+    print("full")
+
+
+polling()
