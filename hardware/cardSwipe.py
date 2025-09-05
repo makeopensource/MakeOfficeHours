43f0cc74e05752f418042b889caa8670288dab00
mariam-al0 <166653306+mariam-al0@users.noreply.github.com>
2025-09-05 16:17:10 -0400

    Card swipe parsed

--- hardware/cardSwipe.py
+++ hardware/cardSwipe.py
@@ -1,26 +1,7 @@
 import os
 
 
-def polling():
-    #file = open('hardware.txt', 'r')
-    #read = file.readlines()
-    #while len(read) == 0:
-        #file = open('hardware.txt', 'r')
-        #read = file.readlines()
-
-    #print('stuff')
-    file = open('hardware.txt', 'r')
-    read = file.readlines()
-    size = len(read)
-
-    while size == 0:
-        file.close()
-        file = open('hardware.txt', 'r')
-        read = file.readlines()
-        size = len(read)
-
-
-    print("full")
-
-
-polling()
+def cardSwipe():
+    name = input().split("^")[1].split(".")[0]
+    #print(name)
+cardSwipe()
