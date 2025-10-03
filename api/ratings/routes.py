bb9f10ecd1b7a476deba88e1a54689b6217de5ad
Jesse Hartloff <hartloff@buffalo.edu>
2025-10-03 16:34:49 -0400

    Remove unused files from previous version of the app

--- api/ratings/routes.py
+++
@@ -1,8 +0,0 @@
-"""Ratings Blueprint for MOH"""
-
-from flask import Blueprint
-
-blueprint = Blueprint("ratings", __name__)
-
-
-# TODO: All the end points for students and TAs
--- api/utils/logging.py
+++
@@ -1,1 +0,0 @@
-"""Logging utils for MOH API server"""
