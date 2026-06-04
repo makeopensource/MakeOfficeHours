40dd6024b47f7b7347d1db3a03c05398e4462dd7
jbcarras <jbcarras@buffalo.edu>
2026-06-04 18:18:10 -0400

    Unbreak the run_local script

--- api/run_local.py
+++ api/run_local.py
@@ -1,11 +1,10 @@
 """Runs the app locally without Gunicorn. To be used for dev and testing"""
 
-from dotenv import load_dotenv
-
-from api.server import create_app
+from dotenv import load_dotenv  # pylint: disable=wrong-import-position
 
 load_dotenv()
 
+from api.server import create_app  # pylint: disable=wrong-import-position
 
 app = create_app()
 app.debug = True
